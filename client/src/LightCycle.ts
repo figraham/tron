a8922596ad480be05a3cbdf9d5c14e835ffe7210
figraham <fi.sage.graham@gmail.com>
2019-05-11 11:30:25 -0600

    sorta working render

--- client/src/LightCycle.ts
+++ client/src/LightCycle.ts
@@ -1,11 +1,15 @@
-import { Vector2 } from "terminaltxt";
+import { Vector2, GraphicsTerminal } from "terminaltxt";
 import { Direction } from "./Direction";
 
 export class LightCycle {
 
   public path: Vector2[];
 
+  public nextDirection: Direction | null;
   public direction: Direction;
+  public previousDirection: Direction;
+  
+  public destroyed: boolean;
 
   constructor(startPosition: Vector2 = new Vector2(), direction: Direction = Direction.RIGHT) {
 
@@ -16,7 +20,10 @@ export class LightCycle {
     this.goRight = this.goRight.bind(this);
 
     // initialization
+    this.nextDirection = null;
     this.direction = direction;
+    this.previousDirection = this.direction;
+    this.destroyed = false;
 
     this.path = [];
     this.path.push(startPosition);
@@ -25,25 +32,25 @@ export class LightCycle {
 
   public goUp(): void {
     if (this.direction !== Direction.DOWN) {
-      this.direction = Direction.UP;
+      this.nextDirection = Direction.UP;
     }
   }
 
   public goDown(): void {
     if (this.direction !== Direction.UP) {
-      this.direction = Direction.DOWN;
+      this.nextDirection = Direction.DOWN;
     }
   }
 
   public goLeft(): void {
     if (this.direction !== Direction.RIGHT) {
-      this.direction = Direction.LEFT;
+      this.nextDirection = Direction.LEFT;
     }
   }
 
   public goRight(): void {
     if (this.direction !== Direction.LEFT) {
-      this.direction = Direction.RIGHT;
+      this.nextDirection = Direction.RIGHT;
     }
   }
 
@@ -52,6 +59,9 @@ export class LightCycle {
   }
 
   public move(): void {
+    if (this.destroyed) {
+      return;
+    }
     let next: Vector2 = Vector2.copy(this.path[this.path.length - 1]);
     // TODO change add when Vector2 is fixed.
     switch (this.direction) {
@@ -69,6 +79,82 @@ export class LightCycle {
         break;
     }
     this.path.push(next);
+  }
+
+  public checkBoundaries(xMin: number, yMin: number, xMax: number, yMax: number): void {
+    if (
+      this.path[this.path.length - 1].x <= xMin ||
+      this.path[this.path.length - 1].y <= yMin ||
+      this.path[this.path.length - 1].x >= xMax ||
+      this.path[this.path.length - 1].y >= yMax
+    ) {
+      this.destroyed = true;
+    }
+  }
+
+  public render(term: GraphicsTerminal): void {
+    if (this.nextDirection !== null) {
+      this.previousDirection = this.direction;
+      this.direction = this.nextDirection;
+      this.nextDirection = null;
+    }
+    let char = '0';
+    switch (this.direction) {
+      case Direction.UP:
+        switch (this.previousDirection) {
+          case Direction.UP:
+          char = '│';
+            break;
+          case Direction.LEFT:
+          char = '└';
+            break;
+          case Direction.RIGHT:
+          char = '┘';
+            break;
+        }
+        break;
+      case Direction.DOWN:
+        switch (this.previousDirection) {
+          case Direction.DOWN:
+          char = '│';
+            break;
+          case Direction.LEFT:
+          char = '┌';
+            break;
+          case Direction.RIGHT:
+          char = '┐';
+            break;
+        }
+        break;
+      case Direction.LEFT:
+        switch (this.previousDirection) {
+          case Direction.LEFT:
+          char = '─';
+            break;
+          case Direction.UP:
+          char = '┐';
+            break;
+          case Direction.DOWN:
+          char = '┘';
+            break;
+        }
+        break;
+      case Direction.RIGHT:
+        char = '─';
+        switch (this.previousDirection) {
+          case Direction.UP:
+          char = '┌';
+            break;
+          case Direction.DOWN:
+          char = '└';
+            break;
+        }
+        break;
+    }
+    term.setCell(char, this.path[this.path.length - 1].x, this.path[this.path.length - 1].y);
+    term.update();
+    this.previousDirection = this.direction;
+    this.move();
   }
 
 }
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,6 +1,10 @@
 import {
   Loop,
-  InputTracker
+  InputTracker,
+  GraphicsTerminal,
+  TerminalConfig,
+  CharacterSet,
+  Vector2
 } from 'terminaltxt';
 import io from 'socket.io-client';
 import { LightCycle } from './LightCycle';
@@ -12,14 +16,27 @@ let socket: SocketIOClient.Socket;
 
 let cycle: LightCycle;
 
+let term: GraphicsTerminal;
+
+const WIDTH = 100;
+const HEIGHT = 50;
+
 function init(): void {
   socket = io.connect('http://localhost:3000');
-  cycle = new LightCycle();
+  term = new GraphicsTerminal(
+    {
+      width: WIDTH,
+      height: HEIGHT,
+    } as TerminalConfig,
+    new CharacterSet(' 0─│┌┐└┘╴╵╶╷')
+  );
+  cycle = new LightCycle(new Vector2(WIDTH / 2, HEIGHT / 2));
   input = userControls(cycle);
   loop.frameRate(10);
 }
 
 function update(): void {
-  cycle.move();
+  cycle.checkBoundaries(0, 0, WIDTH, HEIGHT);
+  cycle.render(term);
   console.log(cycle.path[cycle.path.length -1]);
 }
